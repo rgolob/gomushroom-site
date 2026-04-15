1f226120f2d5d4303c00596c912510d938822621
rgolob <95143515+rgolob@users.noreply.github.com>
2026-04-15 14:23:56 +0200

    Add files via upload

--- zaloga/sw.js
+++ zaloga/sw.js
@@ -1,34 +1,6 @@
-const CACHE = 'gm-v6';
-const ASSETS = ['/zaloga/', '/zaloga/index.html', '/zaloga/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
-  self.skipWaiting();
-});
-
+// SW disabled
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
-
-
-// Minimal SW - no caching, always fetch fresh
-//self.addEventListener('install', e => self.skipWaiting());
-//self.addEventListener('activate', e => {
-//  e.waitUntil(
-//    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-//  );
-//  self.clients.claim();
-//});
-//self.addEventListener('fetch', e => {
-//  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-//});
-
